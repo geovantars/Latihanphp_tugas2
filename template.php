017ea690b1a5a48edf7ca955db08acc2a31e4500
geovantars <geovanta@gmail.com>
2017-10-05 17:56:01 +0700

    Mata kuliah

---
+++ template.php
@@ -0,0 +1,37 @@
+<?php //filename: template.php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kalbis Institute</title>
+	<style>
+		#header{background:#00FF00;}
+		#sidebar{background:yellow;}
+		#footer{background:cyan;}
+	</style>
+</head>
+<body>
+
+<div id="header"><h3>TIKKI | Teknik Informatika Kalbis Institute</h3></div>
+<div id="sidebar">
+	<a href="template.php?page=mahasiswa">Mahasiswa |</a>
+	<a href="template.php?page=dosen">Dosen |</a>
+	<a href="template.php?page=mata_kuliah">Mata Kuliah</a>
+	
+</div>
+
+<div id="konten"> DATA MAHASISWA & DOSEN KALBIS INSTITUTE
+
+<?php 
+include ($_GET['page'] . ".php");
+
+
+?>
+
+</div>
+
+<div id="footer">&copy; 2017 Kalbis Institute</div>
+
+</body>
+</html>
